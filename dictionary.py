0b386503267963777f028b8170ab80f2d37b4052
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-09-27 16:08:17 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+list = [0,1,5,8,9,4]
+list.insert(0,5)
+print(list)
+list.remove(5)
+print(list)
+list.append(6)
+print(list)
+list.sort()
+print(list)
+list.pop()
+print(list)
+list.reverse()
+print(list)
+
+
